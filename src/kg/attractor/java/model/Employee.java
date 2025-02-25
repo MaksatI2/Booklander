ca72ec1668fda792849b3359c1a17772908ce12e
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-25 17:39:55 +0600

    created all the necessary fields for the employee:

--- src/kg/attractor/java/model/Employee.java
+++ src/kg/attractor/java/model/Employee.java
@@ -1,4 +1,24 @@
 package kg.attractor.java.model;
 
+import java.util.List;
+
 public class Employee {
+    private String id;
+    private String name;
+    private List<String> borrowedBooks;
+    private List<String> pastBooks;
+
+    public Employee(String id, String name, List<String> borrowedBooks, List<String> pastBooks) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = borrowedBooks;
+        this.pastBooks = pastBooks;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    public List<String> getBorrowedBooks() { return borrowedBooks; }
+    public List<String> getPastBooks() { return pastBooks; }
 }
+
