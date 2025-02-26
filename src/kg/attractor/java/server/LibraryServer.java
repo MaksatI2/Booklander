5176b895f571fafe5712280b00c68912296e4905
I.Maksat <maksat@MacBookAir.lan>
2025-02-27 01:34:27 +0600

    I got the error "404 Not found"

--- src/kg/attractor/java/server/LibraryServer.java
+++ src/kg/attractor/java/server/LibraryServer.java
@@ -1,5 +1,6 @@
 package kg.attractor.java.server;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import kg.attractor.java.data.LibraryData;
 import kg.attractor.java.handlers.*;
@@ -7,9 +8,12 @@ import kg.attractor.java.handlers.BooksRequestHandler;
 import kg.attractor.java.handlers.BookRequestHandler;
 import kg.attractor.java.handlers.EmployeesRequestHandler;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.net.InetSocketAddress;
+
+import static kg.attractor.java.template.RenderTemplate.sendErrorResponse;
 
 public class LibraryServer {
     private final HttpServer server;
@@ -23,8 +27,18 @@ public class LibraryServer {
         server.createContext("/book/", new BookRequestHandler(dataService));
         server.createContext("/employees", new EmployeesRequestHandler(dataService));
         server.createContext("/employee", new EmployeeRequestHandler(dataService));
-        server.createContext("/", new StaticFileHandler("data/"));
+        server.createContext("/login", new LoginRequestHandler(dataService));
 
+        server.createContext("/", exchange -> {
+            String requestPath = exchange.getRequestURI().getPath();
+            File file = new File("data" + requestPath);
+
+            if (file.exists() && !file.isDirectory()) {
+                new StaticFileHandler("data/").handle(exchange);
+            } else {
+                sendErrorResponse(exchange, ResponseCodes.NOT_FOUND, "404 NOT FOUND");
+            }
+        });
         server.setExecutor(null);
     }
     public void start() {
